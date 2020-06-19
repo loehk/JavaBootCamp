a6758784f13c416a217904ad12538e7774ec603b
loehk <evija.fokrote@gmail.com>
2020-06-19 17:52:50 +0300

    Lesson 1

--- SEF/src/sef/module3/activity/VariableAssignmentActivity.java
+++ SEF/src/sef/module3/activity/VariableAssignmentActivity.java
@@ -13,11 +13,9 @@ public class VariableAssignmentActivity {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		// 1- Declare a variable of type int and assign it default value.
-		// 2- Update the value 
-		// 3- Print updated value to the console
-
+		int number;
+		number=3;
+		System.out.println(number);		
 	}
 
 }
