5ac553b1432cac5f4f04c4ffb0f56659cb08b218
loehk <evija.fokrote@gmail.com>
2020-06-22 17:03:29 +0300

    Second lesson

---
+++ SEF/src/sef/module4/activity/DateActivity.java
@@ -0,0 +1,12 @@
+package sef.module4.activity;
+import java.util.Date;
+
+public class DateActivity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date d = new Date();
+		System.out.println(d);
+	}
+
+}
